22d386339d303515701c8cd8ebe5f54deaff8f6b
Suraj Yadav <qwertypad7612@gmail.com>
2024-11-15 18:44:48 +0530

    New functions added to S3Manager

--- EC2S3Wrapper/S3Manager.py
+++ EC2S3Wrapper/S3Manager.py
@@ -1,6 +1,9 @@
+import os
+from typing import List, Optional
+
 import boto3
 from botocore.exceptions import ClientError, NoCredentialsError
-from typing import List, Optional
+
 
 class S3Manager:
     def __init__(
@@ -80,6 +83,92 @@ class S3Manager:
         except NoCredentialsError:
             print("AWS credentials not found. Ensure they are set correctly.")
             raise
+    
+    def upload_file(
+            self,
+            file_path: str,
+            bucket_name: str,
+            object_name: Optional[str] = None
+        ) -> None:
+        """
+        Uploads a file to the specified S3 bucket.
 
+        Args:
+            file_path (str): The path to the file to upload.
+            bucket_name (str): The name of the S3 bucket.
+            object_name (Optional[str]): The object name in the S3 bucket (defaults to the file name).
 
+        Raises:
+            ClientError: If there is an issue with the file upload request.
+            NoCredentialsError: If AWS credentials are not found or improperly configured.
+        """
+        if object_name is None:
+            object_name = os.path.basename(file_path)
+
+        try:
+            self.s3.upload_file(Filename=file_path, Bucket=bucket_name, Key=object_name)
+            print(f"File '{file_path}' uploaded to S3 as '{object_name}'")
+        except ClientError as e:
+            print(f"Error uploading file: {e}")
+            raise
+        except NoCredentialsError:
+            print("AWS credentials not found. Ensure they are set correctly.")
+            raise
+    
+    def list_objects_in_bucket(self, bucket_name: str) -> Optional[List[str]]:
+        """
+        Lists the object keys in the specified S3 bucket.
+
+        Args:
+            bucket_name (str): The name of the S3 bucket to list the objects from.
+
+        Returns:
+            Optional[List[str]]: A list of object keys in the bucket if successful,
+            or raises an exception if an error occurs.
+
+        Raises:
+            ClientError: If there is an issue with the S3 request, such as permission errors or invalid bucket names.
+            NoCredentialsError: If AWS credentials are not found or are improperly configured.
+        """
+        try:
+            response = self.s3.list_objects_v2(Bucket=bucket_name)
+            object_keys = [obj['Key'] for obj in response.get('Contents', [])]
+            return object_keys
+        except ClientError as e:
+            print(f"Error listing objects in bucket '{bucket_name}': {e}")
+            raise
+        except NoCredentialsError:
+            print("AWS credentials not found. Ensure they are set correctly.")
+            raise
+    
+    def download_file(self, object_name: str, bucket_name: str, file_path: str) -> None:
+        """
+        Downloads a file from the specified S3 bucket to a local path.
+
+        Args:
+            object_name (str): The name of the object in the S3 bucket.
+            bucket_name (str): The name of the S3 bucket.
+            file_path (str): The local path where the file should be saved.
+
+        Raises:
+            ClientError: If there is an error in the S3 request, such as the object not being found or permission issues.
+            NoCredentialsError: If AWS credentials are not found.
+        """
+        # Ensure the directory exists
+        if not os.path.exists(os.path.dirname(file_path)):
+            os.makedirs(os.path.dirname(file_path), exist_ok=True)
+            print(f"{os.path.dirname(file_path)} directory created.")
+
+        try:
+            self.s3.download_file(Bucket=bucket_name, Key=object_name, Filename=file_path)
+            print(f"Downloaded '{object_name}' from S3 bucket '{bucket_name}' to '{file_path}'")
+        except ClientError as e:
+            if e.response['Error']['Code'] == 'NoSuchKey':
+                print(f"Object '{object_name}' not found in bucket '{bucket_name}'")
+            else:
+                print(f"Error downloading file: {e}")
+            raise 
+        except NoCredentialsError:
+            print("AWS credentials not found. Ensure they are set correctly.")
+            raise 
 
